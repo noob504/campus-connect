972f003b210649fee40f0b1438ee5e240c3eb0a5
Aniket <singhaniket@tutanota.com>
2023-03-21 16:21:32 +0530

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!')
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is listening on port 5000')
+})
